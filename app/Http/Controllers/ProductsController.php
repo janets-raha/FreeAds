3ee026c1295770bbb6887953011eb4157a4b88db
Marie-Jeannette Cecora <marie-jeannette.cecora@epitech.eu>
2020-07-08 04:45:41 +0200

    insert data into db

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Products;
 
 class ProductsController extends Controller
 {
     function product(Request $req) {
-        return $req->input();
+        //return $req->input();
+        $product = new Products;
+        $product->name = $req->name;
+        $product->category = $req->category;
+        $product->description = $req->description;
+        $product->condition = $req->condition;
+        $product->price = $req->price;
+        $product->postalcode = $req->postalcode;
+        $product->image = $req->file('image')->store('public\images');
+        echo $product->save();
+
+
     }
 }
